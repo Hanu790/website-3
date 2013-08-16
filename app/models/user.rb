1b28d5228b7a0fd0837a5c072450dae97f1a3cda
PhuongTa <tp.taphuong@gmail.com>
2013-08-17 06:11:21 +0700

    Finish layout and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   # validate :password_confirmation , presence:true
   # has_secure_password
   
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, 
   					format: {with: VALID_EMAIL_REGEX},
   					uniqueness: {case_sensitive: false}
